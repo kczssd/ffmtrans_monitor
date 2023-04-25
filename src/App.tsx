e5a8767857718d9214e1523a4a76c2e71a70dfc3
kczssd <511124893@qq.com>
2023-04-25 23:34:53 +0800

    add: monitor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
-import { useRef, useState } from 'react'
-import Video from "./components/video";
-import './App.css'
+import { useRef, useState, useEffect } from 'react'
+import flvjs from "flv.js"
+import { Input, Button, Layout, Checkbox, Typography, Space } from "@arco-design/web-react"
+import { IconCaretRight, IconCaretLeft, IconSettings } from '@arco-design/web-react/icon';
+import Styles from "./App.module.css"
 
+const Sider = Layout.Sider;
+const Header = Layout.Header;
+const Footer = Layout.Footer;
+const Content = Layout.Content;
+const Group = Checkbox.Group;
 
-function App() {
+const App = () => {
+  const monitor = useRef<HTMLVideoElement>(null);
+  const player = useRef<flvjs.Player>();
+  const [collapsed, setCollapsed] = useState<boolean>(true);
+  const [inputUrl, setInputUrl] = useState("localhost:8080/live?app=ffmtrans&stream=test");
+  const [mediaDataSource, setMediaDataSource] = useState<flvjs.MediaDataSource>({ type: "flv", isLive: true, hasAudio: true, hasVideo: true });
+
+  function play() {
+    console.log("player!", player.current);
+    if (player.current) {
+      console.log("player!")
+      player.current.load();
+      player.current.play();
+    }
+  }
+
+  useEffect(() => {
+    console.log("flvjs.isSupported,", flvjs.isSupported(), monitor.current, mediaDataSource);
+    if (monitor.current) {
+      let flvPlayer = flvjs.createPlayer(mediaDataSource);
+      flvPlayer.attachMediaElement(monitor.current);
+      player.current = flvPlayer;
+    }
+    return () => {
+      player.current?.destroy();
+    }
+  }, [monitor, mediaDataSource])
+
   return (
-    <div className="App">
-      <h1>Play video throught hls</h1>
-      <Video></Video>
-    </div>
-  )
+    <Layout className={Styles.container}>
+      <Header className={Styles.header}>
+        <div style={{ flex: "1 0 auto" }} className={Styles.prefix}>Stream URL:</div>
+        <Input className={Styles.input} size='large' addBefore="http://" placeholder='input rtmp-flv url' allowClear value={inputUrl} onChange={(v) => { setInputUrl(v); setMediaDataSource((o) => ({ ...o, url: v })) }} />
+        <Button type="primary" size="large" onClick={() => { }}>Load</Button>
+        <Button type="primary" status="success" size="large" onClick={() => { }}>Play</Button>
+        <Button size="large" onClick={() => { }}>Pause</Button>
+        <Button size="large" status="danger" onClick={() => { }}>stop</Button>
+      </Header>
+      <Layout className={Styles.main}>
+        <Content className={Styles.content}>
+          <video className={Styles.video} ref={monitor} controls autoPlay></video>
+        </Content>
+        <Sider collapsible trigger={collapsed ? <IconCaretLeft /> : <IconCaretRight />} collapsed={collapsed} onCollapse={() => setCollapsed((v) => !v)}>
+          <p className={Styles.controlTitle}><IconSettings /></p>
+          <div className={Styles.options}>
+            <Checkbox className={Styles.option} onChange={(b) => setMediaDataSource((o) => ({ ...o, isLive: b }))} checked={mediaDataSource.isLive}>Is Stream</Checkbox>
+            <Checkbox className={Styles.option} onChange={(b) => setMediaDataSource((o) => ({ ...o, hasVideo: b }))} checked={mediaDataSource.hasVideo}>Has Video</Checkbox>
+            <Checkbox className={Styles.option} onChange={(b) => setMediaDataSource((o) => ({ ...o, hasAudio: b }))} checked={mediaDataSource.hasAudio}>Has Audio</Checkbox>
+          </div>
+        </Sider>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default App
--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,11 +1,63 @@
-import { useRef, useEffect } from "react"
-import ReactPlayer from "react-player";
+import React,{useRef,useEffect} from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
-export default function Video() {
-
-    return (
-        <div>
-            <ReactPlayer url='http://1.116.116.170/hls/ri.m3u8' />
-        </div>
-    )
+export type Player = ReturnType<typeof videojs>;
+interface VideoJSProps{
+    options:{
+        autoplay?:string | boolean;
+        sources?:any;
+    };
+    onReady:(player:Player)=>unknown;
 }
+
+export const VideoJS = (props:VideoJSProps) => {
+  const videoRef = useRef<HTMLDivElement>(null);
+  const playerRef = useRef<Player|null>(null);
+  const {options, onReady} = props;
+
+  useEffect(() => {
+
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add('vjs-big-play-centered');
+      videoRef.current!.appendChild(videoElement);
+
+      const player = playerRef.current = videojs(videoElement, options, () => {
+        videojs.log('player is ready');
+        onReady && onReady(player);
+      });
+
+    // You could update an existing player in the `else` block here
+    // on prop change, for example:
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+}
+
+export default VideoJS;
